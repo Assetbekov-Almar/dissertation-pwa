1d99d8c0c3c45d810ddb15383eecefbb3be0e13e
Almar <almar.asetbekov@gmail.com>
2021-09-11 19:29:30 +0600

    Feature: Attempted to add install functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,64 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [isInitialMount, setIsInitialMount] = React.useState(true);
+  React.useEffect(() => {
+    if (isInitialMount) {
+      setIsInitialMount(false)
+    } else {
+      const divInstall = document.getElementById('installContainer');
+      const butInstall = document.getElementById('butInstall');
+      butInstall.addEventListener('click', async () => {
+        console.log('👍', 'butInstall-clicked');
+        const promptEvent = window.deferredPrompt;
+        if (!promptEvent) {
+          // The deferred prompt isn't available.
+          return;
+        }
+        // Show the install prompt.
+        promptEvent.prompt();
+        // Log the result
+        const result = await promptEvent.userChoice;
+        console.log('👍', 'userChoice', result);
+        // Reset the deferred prompt variable, since
+        // prompt() can only be called once.
+        window.deferredPrompt = null;
+        // Hide the install button.
+        divInstall.classList.toggle('hidden', true);
+      });
+
+      window.addEventListener('beforeinstallprompt', (event) => {
+        console.log('👍', 'beforeinstallprompt', event);
+        // Stash the event so it can be triggered later.
+        window.deferredPrompt = event;
+        // Remove the 'hidden' class from the install button container
+        divInstall.classList.toggle('hidden', false);
+      });
+      if (window.location.protocol === 'http:') {
+        const requireHTTPS = document.getElementById('requireHTTPS');
+        const link = requireHTTPS.querySelector('a');
+        link.href = window.location.href.replace('http://', 'https://');
+        requireHTTPS.classList.remove('hidden');
+      }
+    }
+  },)
+
+  window.addEventListener("load", () => {
+    if ("serviceWorker" in navigator) {
+      navigator.serviceWorker.register("service-worker.js");
+    }
+  });
+
+  window.addEventListener('appinstalled', (event) => {
+    console.log('👍', 'appinstalled', event);
+    // Clear the deferredPrompt so it can be garbage collected
+    window.deferredPrompt = null;
+  });
+
+
+
   return (
     <div className="App">
       <header className="App-header">
@@ -17,6 +74,15 @@ function App() {
         >
           Learn React
         </a>
+        <p id="requireHTTPS" className="hidden">
+          <b>STOP!</b> This page <b>must</b> be served over HTTPS.
+          Please <a>reload this page via HTTPS</a>.
+        </p>
+        <div id="installContainer" className="hidden">
+          <button id="butInstall" type="button">
+            Install
+          </button>
+        </div>
       </header>
     </div>
   );
