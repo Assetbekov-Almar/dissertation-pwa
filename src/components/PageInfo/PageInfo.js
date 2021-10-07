179d997eeda6b085fca3982b4b8de252ae2a7d5c
almar-a <almar-a@auto.kz>
2021-10-07 19:15:39 +0600

    Feature: Added integration test

--- src/components/PageInfo/PageInfo.js
+++ src/components/PageInfo/PageInfo.js
@@ -4,10 +4,10 @@ import './style.css'
 const PageInfo = () => {
     return (
         <div className="page-info-wrapper">
-            <div className="page-info" id="Home">Something about home</div>
-            <div className="page-info" id="News">Something about news</div>
-            <div className="page-info" id="Contact">Something about contact</div>
-            <div className="page-info" id="About">Something about about</div>
+            <div className="page-info" id="Home" data-testid="home-info">Something about home</div>
+            <div className="page-info" id="News" data-testid="news-info">Something about news</div>
+            <div className="page-info" id="Contact" data-testid="contact-info">Something about contact</div>
+            <div className="page-info" id="About" data-testid="about-info">Something about about</div>
         </div>
     )
 }
--- src/tests/Navigation.test.js
+++ src/tests/Navigation.test.js
@@ -1,12 +1,35 @@
 import React from 'react';
 import Navigation from "../components/Navigation";
-import {render, screen} from "@testing-library/react";
+import {render, screen, fireEvent} from "@testing-library/react";
+import PageInfo from "../components/PageInfo";
 
 describe('Navigation', () => {
     test('should render the links', () => {
         render(<Navigation />);
-        const home = screen.getByText('Home')
-        expect(home).toBeInTheDocument();
+        const home = screen.getByText('Home');
+        const news = screen.getByText('News');
+        const contact = screen.getByText('Contact');
+        const about = screen.getByText('About');
 
+        expect(home).toBeInTheDocument();
+        expect(news).toBeInTheDocument();
+        expect(contact).toBeInTheDocument();
+        expect(about).toBeInTheDocument();
+    })
+
+    test('should show a proper text on click', () => {
+        render(<Navigation />);
+        render(<PageInfo />);
+        fireEvent.click(screen.getByText('Home'));
+       expect(screen.getByTestId('home-info')).toBeInTheDocument();
+
+        fireEvent.click(screen.getByText('News'));
+        expect(screen.getByTestId('news-info')).toBeInTheDocument();
+
+        fireEvent.click(screen.getByText('Contact'));
+        expect(screen.getByTestId('contact-info')).toBeInTheDocument();
+
+        fireEvent.click(screen.getByText('About'));
+        expect(screen.getByTestId('about-info')).toBeInTheDocument();
     })
 })
